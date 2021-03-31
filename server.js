2c5fc3f3102b0ae2019a5fcb67db0eba18e10462
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-03-31 18:12:52 -0400

    psuedo and css linking

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+//set up environment
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
 require('dotenv').config()
 
+//Connect to Mongo
 let db,
     dbConnectionStr = process.env.MONGO_DB,
     dbName = 'this-or-that'
@@ -15,7 +17,7 @@ MongoClient.connect(dbConnectionStr, {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-
+//Middleware
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({
@@ -23,7 +25,7 @@ app.use(express.urlencoded({
 }))
 app.use(express.json())
 
-
+//API
 app.get('/', (request, response) => {
     db.collection('questions').find().toArray()
         .then(data => {
@@ -37,7 +39,13 @@ app.get('/', (request, response) => {
 
 
 
-
+//Server Listen
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+
+// Set up alert/ modal to have user enter name, post that data as a new player object in the players collection 
+// have the questions show up one at a time, on click show the next question and update the player in the player collection with that choice 
+// add admin page with form for me to add new question objects
+// whole lotta css 
